d4acaf68926a5d8dcf93544b87a00f55160469d6
ANFN51 <93614905+ANFN51@users.noreply.github.com>
2024-10-15 23:43:19 -0400

    Update flags.js

--- scripts/flags.js
+++ scripts/flags.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const flagData = [
         { id: "usa-flag", country: "United States", mapSrc: "images/map_usa.png", flagSrc: "images/flag_usa.png" },
         { id: "china-flag", country: "China", mapSrc: "images/map_china.png", flagSrc: "images/flag_china.png" },
-        { id: "india-flag", country: "India", mapSrc: "images/map_india.png", flagSrc: "images/flag_india.png" }
+        { id: "iraq-flag", country: "Iraq", mapSrc: "images/map_iraq.png", flagSrc: "images/flag_iraq.png" }
     ];
 
     function showNextSlide() {
